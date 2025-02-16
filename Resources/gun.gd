6777a7a1d4ea7ffa500152e91dbae615bb42a456
mosiuwu <mosaicawi3@gmail.com>
2025-02-17 00:11:31 +0100

    mamma mia

--- Resources/gun.gd
+++ Resources/gun.gd
@@ -1,22 +1,38 @@
 extends Node2D
-
+@onready var pewpew = $pewpew
 @onready var bullet_scene = preload("res://Resources/bullet.tscn")
 @onready var marker_2d: Marker2D = $Marker2D
 @export var scale_gun: float 
+var pewpewcooldown = true
+var moving = false
+var drift = 0
 
 func _process(delta: float) -> void:
 	look_at(get_global_mouse_position())
 	rotation_degrees = wrap(rotation_degrees, 0, 360)
 	rotation()
 
-	if Input.is_action_just_pressed("shoot"):
+	var jambo = get_parent()
+	if jambo.velocity.length() > 0:
+		moving = true
+	else:
+		moving = false
+
+
+	if Input.is_action_pressed("shoot") and pewpewcooldown == true :
 		shoot()
 
 func shoot():
+	if moving == true:
+		drift = deg_to_rad(randf_range(-15, 15))
+	else:
+		drift = deg_to_rad(randf_range(-5, 5))
+	pewpewcooldown = false
+	pewpew.start()
 	var bullet = bullet_scene.instantiate()
 	get_tree().root.add_child(bullet)
 	bullet.global_position = marker_2d.global_position
-	bullet.rotation = rotation
+	bullet.rotation = rotation + drift
 	
 
 func rotation():
@@ -24,3 +40,6 @@ func rotation():
 		scale.y = -scale_gun
 	else:
 		scale.y = scale_gun
+
+func _on_pewpew_timeout() -> void:
+	pewpewcooldown = true
