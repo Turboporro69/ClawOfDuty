1746fb783c9e8b851646761de588e87dd5fe168b
Jordi Dragan <121635607+Turboporro69@users.noreply.github.com>
2025-02-24 00:53:26 +0100

    iluminación

--- player_2.gd
+++ player_2.gd
@@ -12,24 +12,24 @@ var dash_available = true
 signal player_death
 var player_dead = false
 @export var scale_player : float
-@onready var arm_left = $Skeleton2D/hip/chest/arm_left
-@onready var arm_right = $Skeleton2D/hip/chest/arm_right
-@onready var marker2d_arm : Marker2D = $Skeleton2D/hip/chest/arm_right/hand_right/Marker2D
-@onready var skeleton : Skeleton2D = $Skeleton2D
+@onready var arm_left = $Player/Skeleton/Skeleton2D/hip/chest/arm_left
+@onready var arm_right = $Player/Skeleton/Skeleton2D/hip/chest/arm_right
+@onready var marker2d_arm : Marker2D = $Player/Skeleton/Skeleton2D/hip/chest/arm_right/hand_right/Marker2D
+@onready var skeleton : Skeleton2D = $Player/Skeleton/Skeleton2D
 @onready var gun_handle_marker2d : Marker2D = $Gun/GunHandle
 @onready var gun : Node2D = $Gun
 var flip_h : bool = false
-@onready var polygons : Node2D = $polygons
+@onready var polygons : Node2D = $Player/polygons
 
 func _ready():
-	$polygons.visible = true
+	polygons.visible = true
 	$Gun/Sprite2D.visible = true
 	$CollisionShape2D.disabled = false
 
 func _physics_process(delta: float) -> void:
 	if health == 0 and not player_dead:
 		player_dead = true
-		$polygons.visible = false
+		polygons.visible = false
 		$Gun/Sprite2D.visible = false
 		$CollisionShape2D.disabled = true
 		health_label()
@@ -55,17 +55,6 @@ func _physics_process(delta: float) -> void:
 			
 		health_label()
 		arm_rotation()
-		#if flip_h == true:
-			#gun.rotation_degrees = gun.rotation_degrees * -1
-			#gun.scale.y = gun.scale.y * -1
-			#gun.scale.x = gun.scale_gun * -1
-			#
-		#elif flip_h == false:
-			#gun.rotation_degrees = gun.rotation_degrees
-			#gun.scale.y = gun.scale.y 
-			#gun.scale.x = gun.scale_gun
-		_on_gun_left()
-		_on_gun_right()
 
 func start_wedashing():
 	is_dashing = true
@@ -104,11 +93,11 @@ func arm_rotation():
 	update_gun_position()
 
 func _on_gun_left() -> void:
-	polygons.scale.x = -1
+	$Player.scale.x = -1
 	#rotation_degrees = 180
 	flip_h = true
 #
 func _on_gun_right() -> void:
-	polygons.scale.x = 1
+	$Player.scale.x = 1
 	#rotation_degrees = 0
 	flip_h = false
