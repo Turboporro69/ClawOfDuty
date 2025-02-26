cc38af6b2f029ad9c7df7f291b224db9a2ac1e05
Jordi Dragan <121635607+Turboporro69@users.noreply.github.com>
2025-02-26 20:45:31 +0100

    gcbfvh

--- Scripts/weapons.gd
+++ Scripts/weapons.gd
@@ -1,5 +1,12 @@
 extends Resource
 class_name Weapons
 
+enum Category {Primary, Secondary}
+
 @export var name : String
 @export var texture : Texture2D
+@export var icon : Texture2D
+@export var damage : int
+@export var price : int
+@export var cadence : float
+@export var category : Category
